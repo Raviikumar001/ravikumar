f1c59e68e591ee796d24d1904737a36866dfe93d
ravi <samcollins9899@gmail.com>
2023-10-02 20:05:38 +0530

    added new resume link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,7 +24,7 @@ const handleTwitter= ()=>{
 }
 const handleResume= ()=>{
 
-  window.open("https://drive.google.com/file/d/1a21GJhqGL56JqiPWrq-jQJEpJbZeStCA/view?usp=sharing", "_blank")
+  window.open("https://drive.google.com/file/d/1nvbAUOohbm2nQUp1SwZVHc4pRYTDlsHe/view?usp=sharing", "_blank")
 
 }
 
